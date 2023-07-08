d27084ccd4c5746589cb0d60f02d6787278fca1e
AbhayVerma <pdiwana608@gmail.com>
2023-07-08 15:45:26 +0530

    Leaderboard Optimisation

--- Creating_Signup_Screen_Frontend/controller/premium.js
+++ Creating_Signup_Screen_Frontend/controller/premium.js
@@ -1,31 +1,38 @@
 const Expense = require("../model/expense");
 const User = require("../model/user");
+const sequelize = require("../util/database");
 const database = require("../util/database");
 
 exports.getLeaderBoard = async (req, res) => {
   try {
-    const users = await User.findAll();
-    const expenses = await Expense.findAll();
-    const userAggregatedExpenses = {};
-    expenses.forEach((result) => {
-      if (userAggregatedExpenses[result.userId]) {
-        userAggregatedExpenses[result.userId] += result.amount;
-      } else {
-        userAggregatedExpenses[result.userId] = result.amount;
-      }
-    });
-    console.log(userAggregatedExpenses);
+    //? SQL Joins left join(Uses to Expense)
 
-    const userLeaderBoardDeatils = [];
-    users.forEach((user) => {
-      userLeaderBoardDeatils.push({name: user.name,total_cost: userAggregatedExpenses[user.id]});
+    const leaderBoaderofusers = await User.findAll({
+      attributes: [
+        "id",
+        "name",
+        [sequelize.fn("sum", sequelize.col("amount")), "total_cost"],
+      ],
+      include: [
+        {
+          model: Expense,
+          attributes: [],
+        },
+      ],
+      group: ["id"],
+      order: [["total_cost", "DESC"]],
     });
-    userLeaderBoardDeatils.sort((a,b)=>{
-      b.total_cost-a.total_cost;
-      console.log( b.total_cost-a.total_cost);
-    })
-    console.log(userLeaderBoardDeatils);
-    res.status(200).json(userLeaderBoardDeatils);
+    // console.log(leaderBoaderofusers);
+
+    //* use this type model and find particuler value
+    // const expenses = await Expense.findAll({
+    //   attributes: [
+    //     "userId",
+    //     [sequelize.fn("sum", sequelize.col("amount")), "total_cost"],
+    //   ],
+    //   group: ["userId"],
+    // });
+    res.status(200).json(leaderBoaderofusers);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
